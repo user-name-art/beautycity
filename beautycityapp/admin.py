56775f521aed3ece29f0cb2ed4b165a5f007efbf
E3Tmn <nick1tapinyagin@yandex.ru>
2024-01-24 20:27:23 +0300

    create Master model

--- beautycityapp/admin.py
+++ beautycityapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from beautycityapp.models import Master
 
 # Register your models here.
+admin.site.register(Master)
--- beautycityapp/models.py
+++ beautycityapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Master(models.Model):
+    name = models.CharField('Имя мастера', max_length=50)
+    # studio = models.ForeignKey(
+    #     'Studio',
+    #     on_delete=models.SET_NULL,
+    #     related_name='masters')
+    photo = models.ImageField('Фото')
+
+    def __str__(self):
+        return self.name
