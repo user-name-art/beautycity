4b5d568c01f12b47eba043ba876fb2c842d0459d
E3Tmn <nick1tapinyagin@yandex.ru>
2024-01-24 20:36:40 +0300

    create Studio model

--- beautycityapp/admin.py
+++ beautycityapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from beautycityapp.models import Master
+from beautycityapp.models import Master, Studio
 
 # Register your models here.
 admin.site.register(Master)
+admin.site.register(Studio)
--- beautycityapp/models.py
+++ beautycityapp/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+
+class Studio(models.Model):
+    addres = models.CharField('Адрес студии', max_length=200)
+    photo = models.ImageField('Фото студии')
+
+    def __str__(self):
+        return self.addres
+
 class Master(models.Model):
     name = models.CharField('Имя мастера', max_length=50)
-    # studio = models.ForeignKey(
-    #     'Studio',
-    #     on_delete=models.SET_NULL,
-    #     related_name='masters')
+    studio = models.ForeignKey(
+        'Studio',
+        on_delete=models.SET_NULL,
+        related_name='masters',
+        null=True)
     photo = models.ImageField('Фото')
 
     def __str__(self):
